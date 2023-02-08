f78c36c3c983590b8d8f93190f33b425408b672a
HP <anngophuoc.ltk@gmail.com>
2023-02-08 15:42:47 +0700

    change name CommentsEntity

--- server/memories/src/main/java/com/example/memories/entity/Comments.java
+++
@@ -1,9 +0,0 @@
-package com.example.memories.entity;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-public class Comments {
-}
---
+++ server/memories/src/main/java/com/example/memories/entity/CommentsEntity.java
@@ -0,0 +1,4 @@
+package com.example.memories.entity;
+
+public class CommentsEntity {
+}
