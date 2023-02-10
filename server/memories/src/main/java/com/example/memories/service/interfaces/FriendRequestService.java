1cdb347cddf709261f43732bac204529641486cb
thainta <thai1612200110a@gmail.com>
2023-02-10 16:43:55 +0700

    update friend request

--- server/memories/src/main/java/com/example/memories/service/interfaces/FriendRequestService.java
+++ server/memories/src/main/java/com/example/memories/service/interfaces/FriendRequestService.java
@@ -1,4 +1,16 @@
 package com.example.memories.service.interfaces;
 
+import com.example.memories.model.FriendRequests;
+
+import java.util.List;
+
 public interface FriendRequestService {
+    List<FriendRequests> getAllFriendRequests();
+    FriendRequests getFriendRequestById(long id);
+    List<FriendRequests> getFriendRequestsByUserId(long userId);
+    FriendRequests createFriendRequest(long userId, FriendRequests request);
+    FriendRequests updateFriendRequest(long id, FriendRequests request);
+    FriendRequests deleteFriendRequest(long id);
+    boolean acceptFriendRequest(long id);
+    boolean cancelFriendRequest(long id);
 }
