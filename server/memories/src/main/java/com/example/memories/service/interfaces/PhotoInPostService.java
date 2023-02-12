3eb467f13e0fc132c34e68901fdc66300bb89fcd
thainta <thai1612200110a@gmail.com>
2023-02-12 23:17:36 +0700

    upload

--- server/memories/src/main/java/com/example/memories/service/interfaces/PhotoInPostService.java
+++ server/memories/src/main/java/com/example/memories/service/interfaces/PhotoInPostService.java
@@ -1,12 +1,14 @@
 package com.example.memories.service.interfaces;
 
 import com.example.memories.model.PhotoInPosts;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface PhotoInPostService {
     public List<PhotoInPosts> getAllPhoto();
-    PhotoInPosts createPhotoInPost(PhotoInPosts photoInPosts);
+    PhotoInPosts createPhotoInPost(Long postId, PhotoInPosts photoInPosts, MultipartFile multipartFile)throws IOException;
     PhotoInPosts updatePhoto(Long id, PhotoInPosts photoInPosts);
     PhotoInPosts getPhotoById(Long id);
     Boolean deletePhotoInPost(Long id);
